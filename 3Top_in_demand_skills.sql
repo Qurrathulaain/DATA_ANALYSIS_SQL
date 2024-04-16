6ba8fadf0395d9cecf2b7792077810bdd9b50f4b
Qurrathulaain <qurrath6688@gmail.com>
2024-04-16 22:14:46 +0530

    third push

---
+++ 3Top_in_demand_skills.sql
@@ -0,0 +1,16 @@
+--Top demanded skills
+
+SELECT skills,
+        COUNT(skills_job_dim.job_id) AS skill_in_demand
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON 
+            job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON
+            skills_dim.skill_id = skills_job_dim.skill_id
+WHERE 
+     job_title_short = 'Business Analyst' AND job_work_from_home = TRUE
+GROUP BY 
+    skills
+ORDER BY 
+    skill_in_demand DESC
+LIMIT 5;
