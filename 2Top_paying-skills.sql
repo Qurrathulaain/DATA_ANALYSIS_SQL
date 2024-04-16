ecb37a740b0541a8635e34ff69a05fd13d7213f7
Qurrathulaain <qurrath6688@gmail.com>
2024-04-16 20:56:21 +0530

    third commit

---
+++ 2Top_paying-skills.sql
@@ -0,0 +1,30 @@
+
+--Query to find the top paying skills.
+
+WITH top_paying_skills AS
+    (
+    SELECT 
+            job_id,
+            name AS company_name,
+            job_title_short,
+            salary_year_avg
+    FROM job_postings_fact
+    LEFT JOIN company_dim ON
+                    job_postings_fact.company_id = company_dim.company_id
+    WHERE
+            job_title_short = 'Data Analyst' AND
+            job_location = 'Anywhere' AND
+            salary_year_avg IS NOT NULL
+    ORDER BY 
+            salary_year_avg DESC
+    LIMIT 10
+    )
+
+SELECT top_paying_skills.*,
+       skills
+FROM top_paying_skills
+INNER JOIN skills_job_dim ON
+            top_paying_skills.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON
+            skills_job_dim.skill_id = skills_dim.skill_id
+ORDER BY salary_year_avg DESC;
